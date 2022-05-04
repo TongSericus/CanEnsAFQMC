ae65f25851b14c5e86d7ff5fb4cc7831cd30f9e0
TongSericus <tong_shen1@brown.edu>
2022-05-04 16:59:25 -0400

    add autocorrelation estimates

--- src/analysis/reblocking.jl
+++ src/analysis/reblocking.jl
@@ -1,8 +1,15 @@
 """
+    Autocorrelation function estimate and sample reblocking
     Steal from https://dfm.io/posts/autocorr/ and rewrite into Julia form
 """
-using FFTW: fft, ifft
+
+import FFTW: fft, ifft
+import Statistics: mean, std
+
 function next_pow_two(n::Int64)
+    """
+    Returns the next power of two greater than or equal to n
+    """
     i = 1
     while i < n
         i = i << 1
@@ -11,23 +18,49 @@ function next_pow_two(n::Int64)
 end
 
 function autocorr_func_1d(x::Vector{T}) where {T <: FloatType}
-    n = next_pow_two(x)
-    x = vcat(x .- mean(x), zeros(T, 2 * n - length(x)))
+    """
+    Estimate the normalized autocorrelation function of a 1-D series
+    """
+    n = next_pow_two(length(x))
+    x_expand = vcat(x .- mean(x), zeros(T, 2 * n - length(x)))
     # Compute the FFT and then (from that) the auto-correlation function
-    f = fft(x)
+    f = fft(x_expand)
     acf = real(ifft(f .* conj(f)))[1 : length(x)]
     acf /= acf[1]
     return acf
 end
 
-function autocorr_func_test(x::Vector{T}) where {T <: FloatType}
-    N = length(x)
-    mu = mean(x)
-    Ctau = zeros(T, N)
-    for tau = 0 : N - 1
-        for n = 1 : N - tau
-            Ctau[tau + 1] += (x[n] - mu) * (x[n + tau] - mu)
-        end
+# Automated windowing procedure following Sokal (1989)
+auto_window(τf, c) = argmin([i for i = 0 : length(τf) - 1] .< c * τf)
+
+function autocorr_gw2010(x::Vector{T}, c::Float64 = 5.0) where {T <: FloatType}
+    """
+    Following the suggestion from Goodman & Weare (2010)
+    """
+    acf = autocorr_func_1d(x)
+    τf = 2 * cumsum(acf) .- 1
+    return τf[auto_window(τf, c)]
+end
+
+function reblock(x::Vector{T}) where {T <: FloatType}
+    """
+    Compute the reblocked sample averages and error bars using ACF
+    """
+    Nmax = convert(Int64,floor(log2(length(x))))
+    τ = Vector{T}()
+    for i = 0 : Nmax - 1
+        n = convert(Int64, floor(length(x) / 2 ^ i))
+        push!(τ, autocorr_gw2010(x[1 : n]))
     end
-    return Ctau
+
+    block_size = convert(Int64, round(maximum(τ)))
+    nblocks = div(length(x), block_size)
+    xblocked = Vector{T}()
+    for i = 1 : nblocks
+        offset = (i - 1) * block_size + 1
+        push!(xblocked, mean(x[offset : min(offset + block_size, end)]))
+    end
+
+    return mean(xblocked), std(xblocked) / sqrt(nblocks)
+
 end
