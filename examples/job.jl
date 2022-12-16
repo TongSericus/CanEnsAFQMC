5cf95e877ffbd5156a75fa646bc8c73e1699560e
TongSericus <tong_shen1@brown.edu>
2022-12-15 20:47:56 -0500

    removed some old scripts

--- examples/job.jl
+++
@@ -1,8 +0,0 @@
-#include("mcrun.jl")
-include("benchmark_run.jl")
-
-const worker_id = get(ENV, "SLURM_ARRAY_TASK_ID", 1)
-@show worker_id
-
-#replica_run(worker_id, system, qmc)
-mcrun(worker_id, system, qmc)
--- examples/job_analysis.jl
+++
@@ -1,8 +0,0 @@
-include("data_analysis.jl")
-
-#const worker_id = get(ENV, "SLURM_ARRAY_TASK_ID", 1)
-#@show worker_id
-
-const filenames = ["Replica_Lx4_Ly2_U6.0_beta2.0_$(id).jld" for id = 1 : 4]
-
-data_analysis_etgent(filenames)
