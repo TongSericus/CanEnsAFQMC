b361b1cd24da0a3ab22398f775527d989dc05f16
TongSericus <tong_shen1@brown.edu>
2023-07-17 00:44:40 -0400

    removed the gce module and some other redundant files

--- src/CanEnsAFQMC.jl
+++ src/CanEnsAFQMC.jl
@@ -1,60 +1,32 @@
 module CanEnsAFQMC
 
-using Reexport: @reexport
-@reexport using DoubleFloats, LinearAlgebra, StableLinearAlgebra, Statistics, Random, FFTW
+    using Reexport: @reexport
+    @reexport using LinearAlgebra, StableLinearAlgebra, Random, FFTW
 
-export QMC, GeneralMeasure, EtgMeasure
-export System, Hubbard, nudge_system
-include("./base/systems.jl")
-include("./base/matrix_generator.jl")
-include("./base/variable.jl")
+    export System, Hubbard, GenericHubbard
+    include("./base/systems.jl")
+    export hopping_matrix_Hubbard_1d, hopping_matrix_Hubbard_2d
+    include("./base/matrix_generator.jl")
+    export QMC
+    include("./base/variable.jl")
 
-export sgn, fermilevel, poissbino, sum_antidiagonal
-include("./utils/quickmath.jl")
+    export sgn, fermilevel, poissbino
+    include("./utils/quickmath.jl")
 
-export pf_recursion, occ_recursion
-include("./utils/ce_recursion.jl")
+    export compute_pf_recursion, compute_occ_recursion
+    include("./utils/ce_recursion.jl")
 
-export pf_projection, occ_projection
-include("./utils/ce_projection.jl")
+    #export pf_projection, occ_projection
+    #include("./utils/ce_projection.jl")
 
-export UDT, UDR, UDTlr
-export QR_lmul, QR_lmul!, QR_rmul, QR_rmul!, 
-    QR_sum, QR_merge, QR_merge!, QR_update,
-    inv_IpμA!
-include("./utils/linalg.jl")
-include("./utils/linalg_lowrank.jl")
+    export LDRLowRank
+    include("./utils/linalg.jl")
+    include("./utils/linalg_lowrank.jl")
 
-export Walker, Cluster, compute_PF
-include("./propagation/walker.jl")
-include("./propagation/operations.jl")
-export sweep!, reverse_sweep!
-include("./propagation/metropolis.jl")
-include("./propagation/constraint_path.jl")
-include("./propagation/replica.jl")
-
-export GCWalker, HubbardGCWalker, GeneralGCWalker
-include("./gce/walker.jl")
-include("./gce/operations.jl")
-include("./gce/propagation.jl")
-include("./gce/replica.jl")
-
-export generate_DFTmat, generate_DFTmats
-include("./measurements/measurements.jl")
-export DensityMatrices, fill_DM!
-include("./measurements/density_matrix.jl")
-export measure_Energy
-include("./measurements/energy.jl")
-export measure_HeatCapacity_denom, measure_HeatCapacity_num
-include("./measurements/heat_capacity.jl")
-export measure_MomentumDist
-include("./measurements/momentum_dist.jl")
-export measure_ChargeCorr, measure_SpinCorr
-include("./measurements/charge_correlation.jl")
-export measure_TransitProb
-include("./measurements/transition_probability.jl")
-
-export MuTuner, dynamical_tuning
-include("./gce/dynamical_tuning.jl")
+    export Walker, compute_pf
+    include("./propagation/walker.jl")
+    include("./propagation/operations.jl")
+    export sweep!, sweep!_asymmetric
+    include("./propagation/metropolis.jl")
 
 end
